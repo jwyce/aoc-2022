d958d3a6953fe2c9a4a87378b7a3ef33a95b0258
Jared Wyce <wycejared@gmail.com>
2022-12-03 23:51:38 -0500

    🙈 remove commented code

--- 3/a.ts
+++ 3/a.ts
@@ -33,35 +33,3 @@ console.log(
 	'answer',
 	sacks.reduce((agg, sum) => agg + sum)
 );
-
-// export const threeElfGroupRucksack = (file: string) => {
-//   const input = fs.readFileSync(file, 'utf8');
-//   const sacks = input
-//     .split(/\n/)
-//     .map((rucksack) =>
-//       rucksack
-//         .split('')
-//         .map((char) =>
-//           char === char.toLowerCase()
-//             ? char.charCodeAt(0) - 96
-//             : char.charCodeAt(0) - 64 + 26
-//         )
-//     );
-
-//   console.log('sacks', sacks);
-
-//   const output: number[] = [];
-//   for (let i = 0; i < sacks.length; i += 3) {
-//     const comp1 = sacks[i];
-//     const comp2 = sacks[i + 1];
-//     const comp3 = sacks[i + 2];
-
-//     const commonItems = intersect(intersect(comp1, comp2), comp3);
-//     console.log('commonItems', commonItems);
-//     output.push(commonItems[0]);
-//   }
-
-//   console.log('pre summed output', output);
-
-//   return output.reduce((agg, sum) => agg + sum);
-// };
